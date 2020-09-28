5c3b128ebe344b2a69821fb1b6ba16db5d400c7c
Anastasia <anastasiaskbb@mail.ru>
2020-09-28 14:59:27 +0400

    v1

---
+++ lab1MT/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace lab1MT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveUsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(716, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveUsToolStripMenuItem
+            // 
+            this.saveUsToolStripMenuItem.Name = "saveUsToolStripMenuItem";
+            this.saveUsToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.saveUsToolStripMenuItem.Text = "Save us";
+            this.saveUsToolStripMenuItem.Click += new System.EventHandler(this.saveUsToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(157, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(386, 358);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Replacing channels";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 537);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveUsToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lab1MT/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1MT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct Pixel
+        {
+            public byte Blue;
+            public byte Green;
+            public byte Red;
+        }
+
+            private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog files = new OpenFileDialog();
+            files.Filter = "Image Files(*.BMP; *.JPG; *.GIF; *.PNG)| *.BMP; *.JPG; *.GIF; *.PNG | All files(*.*) | *.* ";
+            if(files.ShowDialog()==DialogResult.OK)
+            {
+                try 
+                {
+                    pictureBox1.Image = new Bitmap(files.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Invalid file format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void saveUsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image!=null)
+            {
+                SaveFileDialog files = new SaveFileDialog();
+                files.Filter = "Image Files(*.BMP; *.JPG; *.GIF; *.PNG)| *.BMP; *.JPG; *.GIF; *.PNG | All files(*.*) | *.* ";
+
+                if(files.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        pictureBox1.Image.Save(files.FileName);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Invalid file format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap Image = new Bitmap(pictureBox1.Image);
+            Bitmap ResultImage = new Bitmap(Image.Width, Image.Height);
+            unsafe
+            {
+                var oneBits = Image.LockBits(new Rectangle(0, 0, Image.Width, Image.Height), ImageLockMode.ReadOnly, Image.PixelFormat);
+                var twoBits = ResultImage.LockBits(new Rectangle(0, 0, ResultImage.Width, ResultImage.Height), ImageLockMode.WriteOnly, ResultImage.PixelFormat);
+                int padding = twoBits.Stride - (ResultImage.Width * sizeof(Pixel));
+                int width = ResultImage.Width;
+                int height = ResultImage.Height;
+                Parallel.For(0, Image.Width * Image.Height, i =>
+                {
+                    Pixel* pxOne = (Pixel*)((byte*)oneBits.Scan0 + i * sizeof(Pixel));
+                    byte* ptr = (byte*)twoBits.Scan0;
+                    for (int j = 0; j < height; j++)
+                    {
+                        for(int k = 0; k < width; k++) 
+                        {
+                            
+                            Pixel* pxTwo = (Pixel*)((byte*)twoBits.Scan0 + i * sizeof(Pixel));
+                            pxOne->Red = pxTwo->Green;
+                            pxOne->Green = pxTwo->Blue;
+                            pxOne->Blue = pxTwo->Red;
+                            ptr += sizeof(Pixel);
+                        }
+                        ptr += padding;
+                    }
+                });
+                Image.UnlockBits(oneBits);
+                ResultImage.UnlockBits(twoBits);
+                pictureBox1.Image = ResultImage;
+
+            }
+        }
+    }
+}
